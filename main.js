64cf48956800d3a99e5e392869efee4eb3511d3b
anand <anandsngh.6575@gmail.com>
2019-06-18 15:09:08 +0530

    fixed

---
+++ main.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+var path = require('path');
+var html = fs.readFileSync('./po.html', 'utf8');
+const base = path.resolve('static'); //  just relative path to absolute path
+// you have to set 'file://'
+
+
+var options = {base: `file://${base}`, format: 'letter' };
+
+pdf.create(html, options).toFile('./files.pdf', function(err, res) {
+ if (err) return console.log(err);
+ console.log(res); // { filename: '/app/businesscard.pdf' }
+});
